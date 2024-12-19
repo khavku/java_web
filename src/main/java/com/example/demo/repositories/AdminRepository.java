e940ce0bb1f9ed027fc8f94472f27acf9a2dd82b
khavku <khatranhonda@gmail.com>
2024-12-20 00:20:29 +0700

    kha

---
+++ src/main/java/com/example/demo/repositories/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.entities.Admin;
+
+public interface AdminRepository extends CrudRepository<Admin, Integer> {
+	public Admin findByAdminEmail(String email);
+}
